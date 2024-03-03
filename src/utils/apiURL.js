d5389012d3f0d93720143f77e4356352ec57e3f6
mazkar27 <mazkar27@gmail.com>
2024-03-03 13:36:27 +0700

    .

--- src/utils/apiURL.js
+++ src/utils/apiURL.js
@@ -2,6 +2,5 @@ export const baseUrl = {
   // URL: "http://193.163.127.3:88/",
   // URL: "https://www.mamachefapi-dev.my.id/",
   URL: "https://dev-api.mamachefs.com/",
-
   // URL: "https://api.mamachefs.com/",
 };
